c2d6e2597af30e096ad070127a7104be64e57911
Fabian Vowie <crymaprivat@gmail.com>
2020-10-06 20:12:46 +0200

    Add logger

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,16 +4,20 @@ extern crate winapi;
 mod memory;
 mod utility;
 
+use log::{debug, info};
 use memory::{address_fill, get_pattern, get_pattern_rip};
-use std::io::prelude::*;
+
+make_entrypoint!(entrypoint);
 
 fn entrypoint() {
-    let mut file = std::fs::File::create("E:\\Prototypes\\CryV\\tmp.txt").unwrap();
+    create_logger().expect("Something went wrong while creating the logger!");
 
     let base = unsafe { winapi::um::libloaderapi::GetModuleHandleA(std::ptr::null()) };
 
-    file.write(format!("Base: {:p}\n", base).as_bytes())
-        .unwrap();
+    info!("--------------------------");
+    info!("Starting CryV");
+
+    debug!("Base: {:p}", base);
 
     let logos = get_pattern("70 6C 61 74 66 6F 72 6D 3A".to_string(), 0);
     let game_legal_skip = get_pattern("72 1F E8 ? ? ? ? 8B 0D".to_string(), 0);
@@ -24,16 +28,29 @@ fn entrypoint() {
     address_fill(logos, 1, 0xC3);
     address_fill(story_mode_skip, 6, 0x90);
 
-    file.write(format!("Logos: {:p}\n", logos).as_bytes())
-        .unwrap();
-    file.write(format!("GameLegalSkip: {:p}\n", game_legal_skip).as_bytes())
-        .unwrap();
-    file.write(format!("FrameCount: {:p}\n", frame_count).as_bytes())
-        .unwrap();
-    file.write(format!("StoryModeSkip: {:p}\n", story_mode_skip).as_bytes())
-        .unwrap();
+    debug!("Logos: {:p}", logos);
+    debug!("GameLegalSkip: {:p}", game_legal_skip);
+    debug!("FrameCount: {:p}", frame_count);
+    debug!("StoryModeSkip: {:p}", story_mode_skip);
 
-    file.write(format!("Done!\n").as_bytes()).unwrap();
+    info!("Successfully started CryV");
 }
 
-make_entrypoint!(entrypoint);
+fn create_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file(r"E:\Prototypes\CryV\cryv.log")?)
+        .apply()?;
+
+    Ok(())
+}
