dc7340d02984a2c09ee9709bc5dca9b8dcdfcb67
Fabian Vowie <crymaprivat@gmail.com>
2020-12-17 19:43:04 +0100

    Use primarily high level imgui instead of imgui_sys

--- client/src/imgui.rs
+++ client/src/imgui.rs
@@ -1,6 +1,6 @@
 use std::sync::{atomic::AtomicBool, Mutex};
 
-use once_cell::sync::Lazy;
+use once_cell::sync::{Lazy, OnceCell};
 use shared::bevy::ecs::{Entity, Resources, World};
 use winapi::{
     shared::minwindef::LPARAM, shared::minwindef::UINT, shared::minwindef::WPARAM,
@@ -10,21 +10,50 @@ use winapi::{
 use crate::{entities::EntityHandle, wrapped_natives::entities};
 
 static mut SHOW_CURSOR: AtomicBool = AtomicBool::new(false);
-static mut HAS_INIT: bool = false;
 static LOG_BUFFER: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(Vec::new()));
 
-static SPAWN_VEHICLE_MODAL: &str = "Spawn Vehicle\0";
+static mut CTX: OnceCell<Mutex<ContextWrapper>> = OnceCell::new();
+static mut IMGUI_DATA: OnceCell<ImguiData> = OnceCell::new();
 
-pub(super) fn d3d11_present(
+#[derive(Debug)]
+struct ContextWrapper {
+    pub ctx: imgui::Context,
+}
+
+#[derive(Debug)]
+struct ImguiData {
+    pub show_commands: bool,
+    pub show_logs: bool,
+    pub vehicle_model: imgui::ImString,
+    pub primary_color: i32,
+    pub secondary_color: i32,
+}
+
+impl Default for ImguiData {
+    fn default() -> Self {
+        ImguiData {
+            show_commands: false,
+            show_logs: false,
+            vehicle_model: imgui::ImString::with_capacity(32),
+            primary_color: 112,
+            secondary_color: 112,
+        }
+    }
+}
+
+unsafe impl Send for ContextWrapper {}
+
+fn get_context(
     device: *mut hook::winapi::um::d3d11::ID3D11Device,
     context: *mut hook::winapi::um::d3d11::ID3D11DeviceContext,
-) {
+) -> &'static Mutex<ContextWrapper> {
     unsafe {
-        if HAS_INIT == false {
+        CTX.get_or_init(|| {
             let window_name = std::ffi::CString::new("grcWindow").unwrap();
             let window = FindWindowA(window_name.as_ptr(), std::ptr::null());
 
-            imgui_sys::igCreateContext(std::ptr::null_mut());
+            let ctx = imgui::Context::create();
+
             imgui_sys::ImGui_ImplDX11_Init(
                 device as *mut imgui_sys::ID3D11Device,
                 context as *mut imgui_sys::ID3D11DeviceContext,
@@ -33,20 +62,205 @@ pub(super) fn d3d11_present(
 
             add_log("Initialized D3D11_PRESENT hook");
 
-            HAS_INIT = true;
+            Mutex::new(ContextWrapper { ctx })
+        })
+    }
+}
+
+fn get_data<'a>() -> &'a mut ImguiData {
+    unsafe {
+        if let None = IMGUI_DATA.get() {
+            IMGUI_DATA.set(ImguiData::default()).unwrap();
         }
 
+        IMGUI_DATA.get_mut().unwrap()
+    }
+}
+
+pub(super) fn d3d11_present(
+    device: *mut hook::winapi::um::d3d11::ID3D11Device,
+    context: *mut hook::winapi::um::d3d11::ID3D11DeviceContext,
+) {
+    let mut wrapper = get_context(device, context).lock().unwrap();
+    let data = get_data();
+
+    unsafe {
         imgui_sys::ImGui_ImplDX11_NewFrame();
         imgui_sys::ImGui_ImplWin32_NewFrame();
-        imgui_sys::igNewFrame();
+    };
 
-        draw_imgui();
+    let ui = wrapper.ctx.frame();
 
-        imgui_sys::igRender();
+    draw(&ui, data);
+
+    unsafe {
+        (*imgui_sys::igGetIO()).MouseDrawCursor = *SHOW_CURSOR.get_mut();
+        let draw_data = ui.render() as *const imgui::DrawData;
         imgui_sys::ImGui_ImplDX11_RenderDrawData(
-            imgui_sys::igGetDrawData() as *mut std::ffi::c_void
+            (draw_data as *const std::ffi::c_void) as *mut std::ffi::c_void,
         );
     };
+}
+
+fn draw(ui: &imgui::Ui, data: &mut ImguiData) {
+    ui.main_menu_bar(|| {
+        ui.menu(imgui::im_str!("CryV"), true, || {});
+
+        ui.menu(imgui::im_str!("Windows"), true, || {
+            if imgui::MenuItem::new(imgui::im_str!("Commands"))
+                .selected(data.show_commands)
+                .build(&ui)
+            {
+                data.show_commands = !data.show_commands;
+            }
+
+            if imgui::MenuItem::new(imgui::im_str!("Logs"))
+                .selected(data.show_logs)
+                .build(&ui)
+            {
+                data.show_logs = !data.show_logs;
+            }
+        });
+    });
+
+    if data.show_commands {
+        imgui::Window::new(imgui::im_str!("Commands"))
+            .size([300.0, 100.0], imgui::Condition::FirstUseEver)
+            .build(&ui, || {
+                if ui.button(imgui::im_str!("Spawn Vehicle"), [0.0, 0.0]) {
+                    ui.open_popup(imgui::im_str!("Spawn Vehicle"));
+                }
+
+                ui.same_line_with_spacing(0.0, -1.0);
+
+                if ui.button(imgui::im_str!("Delete Vehicle"), [0.0, 0.0]) {
+                    crate::thread_jumper::run(Box::new(|world, _| {
+                        let player_ped_id = hook::natives::player::player_ped_id();
+                        let is_in_vehicle =
+                            hook::natives::ped::is_ped_in_any_vehicle(player_ped_id, false);
+
+                        if is_in_vehicle == false {
+                            add_log("You are not in any vehicle");
+
+                            return;
+                        }
+
+                        let mut vehicle_id =
+                            hook::natives::ped::get_vehicle_ped_is_in(player_ped_id, false);
+                        // Copy the vehicle id, as GTA5 will clear the current one, after deleting the entity
+                        let vehicle_id_copy = vehicle_id.clone();
+
+                        entities::delete_entity(&mut vehicle_id);
+
+                        let existing_entities = world.query::<(Entity, &EntityHandle)>();
+
+                        let mut found_entity: Option<Entity> = None;
+
+                        for (entity, entity_data) in existing_entities {
+                            if entity_data.handle != vehicle_id_copy {
+                                continue;
+                            }
+
+                            found_entity = Some(entity);
+                        }
+
+                        if let Some(x) = found_entity {
+                            world.despawn(x).unwrap();
+
+                            add_log("Vehicle has been deleted");
+                        }
+                    }));
+                }
+
+                if ui.button(imgui::im_str!("Load Cayo Perico"), [0.0, 0.0]) {
+                    crate::thread_jumper::run(Box::new(|_, _| {
+                        let heist_island_cstring = std::ffi::CString::new("HeistIsland").unwrap();
+                        hook::natives::dlc::load_cayo_perico(&heist_island_cstring, true);
+                    }));
+                }
+
+                ui.same_line_with_spacing(0.0, -1.0);
+
+                if ui.button(imgui::im_str!("Unload Cayo Perico"), [0.0, 0.0]) {
+                    crate::thread_jumper::run(Box::new(|_, _| {
+                        let x = std::ffi::CString::new("HeistIsland").unwrap();
+                        hook::natives::dlc::load_cayo_perico(&x, false);
+                    }));
+                }
+
+                if ui.button(imgui::im_str!("Teleport to Cayo Perico"), [0.0, 0.0]) {
+                    crate::thread_jumper::run(Box::new(|_, _| {
+                        let player_ped = hook::natives::player::player_ped_id();
+
+                        hook::natives::entity::set_entity_coords_no_offset(
+                            player_ped, 4840.571, -5174.425, 2.0, false, false, false,
+                        );
+                    }));
+                }
+
+                ui.popup_modal(imgui::im_str!("Spawn Vehicle"))
+                    .always_auto_resize(true)
+                    .build(|| {
+                        ui.input_text(imgui::im_str!("Vehicle Model"), &mut data.vehicle_model)
+                            .build();
+
+                        ui.input_int(imgui::im_str!("Primary Color"), &mut data.primary_color)
+                            .build();
+
+                        ui.input_int(imgui::im_str!("Secondary Color"), &mut data.secondary_color)
+                            .build();
+
+                        ui.separator();
+
+                        if ui.button(imgui::im_str!("Done"), [120.0, 0.0]) {
+                            let model_name = data.vehicle_model.to_string();
+                            let primary_color = data.primary_color;
+                            let secondary_color = data.secondary_color;
+
+                            crate::thread_jumper::run(Box::new(move |world, _| {
+                                let player_ped_id = hook::natives::player::player_ped_id();
+                                let position =
+                                    hook::natives::entity::get_entity_coords(player_ped_id, true);
+                                let rotation =
+                                    hook::natives::entity::get_entity_rotation(player_ped_id, 2);
+
+                                let (handle, model, transform, vehicle) =
+                                    crate::entities::create_vehicle_with_model_name(
+                                        &model_name,
+                                        position,
+                                        rotation,
+                                        primary_color,
+                                        secondary_color,
+                                    );
+
+                                hook::natives::ped::set_ped_into_vehicle(
+                                    player_ped_id,
+                                    handle.handle,
+                                    -1,
+                                );
+
+                                world.spawn((handle, model, transform, vehicle));
+
+                                add_log(&format!(
+                                    "Spawned vehicle ({})",
+                                    model_name.to_uppercase()
+                                ));
+                            }));
+
+                            ui.close_current_popup();
+                        }
+                    });
+            });
+    }
+
+    if data.show_logs {
+        imgui::Window::new(imgui::im_str!("Logs"))
+            .size([300.0, 100.0], imgui::Condition::FirstUseEver)
+            .build(&ui, || {
+                let buffer = LOG_BUFFER.lock().unwrap();
+                ui.text(imgui::ImString::new(buffer.join("\n")));
+            });
+    }
 }
 
 pub(super) fn wndproc(hwnd: HWND, message: UINT, w_param: WPARAM, l_param: LPARAM) {
@@ -73,168 +287,6 @@ pub(super) fn handle_cursor(_world: &mut World, _resources: &mut Resources) {
 
     if unsafe { *SHOW_CURSOR.get_mut() } {
         hook::natives::pad::disable_all_control_actions(0);
-    }
-}
-
-unsafe fn draw_imgui() {
-    (*imgui_sys::igGetIO()).MouseDrawCursor = *SHOW_CURSOR.get_mut();
-
-    let window_title = std::ffi::CString::new("CryV - Debug").unwrap();
-    imgui_sys::igSetNextWindowSize(
-        imgui_sys::ImVec2::new(350.0, 300.0),
-        imgui_sys::ImGuiCond_Appearing,
-    );
-
-    if imgui_sys::igBegin(window_title.as_ptr(), std::ptr::null_mut(), 0) {
-        draw_commands();
-        draw_log();
-    }
-
-    draw_modals();
-
-    imgui_sys::igEnd();
-}
-
-unsafe fn draw_commands() {
-    let commands_text = std::ffi::CString::new("Commands").unwrap();
-    if imgui_sys::igCollapsingHeaderBoolPtr(commands_text.as_ptr(), std::ptr::null_mut(), 0) {
-        let spawn_vehicle_text = std::ffi::CString::new("Spawn Vehicle").unwrap();
-        if imgui_sys::igButton(
-            spawn_vehicle_text.as_ptr(),
-            imgui_sys::ImVec2::new(0.0, 0.0),
-        ) {
-            imgui_sys::igOpenPopup(SPAWN_VEHICLE_MODAL.as_ptr() as *const i8, 0);
-        }
-
-        imgui_sys::igSameLine(0.0, -1.0);
-
-        let delete_vehicle_text = std::ffi::CString::new("Delete Vehicle").unwrap();
-        if imgui_sys::igButton(
-            delete_vehicle_text.as_ptr(),
-            imgui_sys::ImVec2::new(0.0, 0.0),
-        ) {
-            crate::thread_jumper::run(|world, _| {
-                let player_ped_id = hook::natives::player::player_ped_id();
-                let is_in_vehicle = hook::natives::ped::is_ped_in_any_vehicle(player_ped_id, false);
-
-                if is_in_vehicle == false {
-                    add_log("You are not in any vehicle");
-
-                    return;
-                }
-
-                let mut vehicle_id =
-                    hook::natives::ped::get_vehicle_ped_is_in(player_ped_id, false);
-                // Copy the vehicle id, as GTA5 will clear the current one, after deleting the entity
-                let vehicle_id_copy = vehicle_id.clone();
-
-                entities::delete_entity(&mut vehicle_id);
-
-                let existing_entities = world.query::<(Entity, &EntityHandle)>();
-
-                let mut found_entity: Option<Entity> = None;
-
-                for (entity, entity_data) in existing_entities {
-                    if entity_data.handle != vehicle_id_copy {
-                        continue;
-                    }
-
-                    found_entity = Some(entity);
-                }
-
-                if let Some(x) = found_entity {
-                    world.despawn(x).unwrap();
-
-                    add_log("Vehicle has been deleted");
-                }
-            });
-        }
-    }
-}
-
-unsafe fn draw_log() {
-    let log_text = std::ffi::CString::new("Log").unwrap();
-    if imgui_sys::igCollapsingHeaderBoolPtr(log_text.as_ptr(), &mut true, 0) {
-        let buffer = LOG_BUFFER.lock().unwrap();
-        let text = buffer.join("\n");
-        let text_cstring = std::ffi::CString::new(text.clone()).unwrap();
-        imgui_sys::igTextUnformatted(
-            text_cstring.as_ptr(),
-            text_cstring
-                .as_ptr()
-                .add(text.len() * std::mem::size_of::<std::os::raw::c_char>()),
-        );
-    }
-}
-
-unsafe fn draw_modals() {
-    let imgui_io = *imgui_sys::igGetIO();
-    let center = imgui_sys::ImVec2::new(imgui_io.DisplaySize.x * 0.5, imgui_io.DisplaySize.y * 0.5);
-    imgui_sys::igSetNextWindowPos(
-        center,
-        imgui_sys::ImGuiCond_Appearing,
-        imgui_sys::ImVec2::new(0.5, 0.5),
-    );
-
-    if imgui_sys::igBeginPopupModal(
-        SPAWN_VEHICLE_MODAL.as_ptr() as *const i8,
-        std::ptr::null_mut(),
-        imgui_sys::ImGuiWindowFlags_AlwaysAutoResize,
-    ) {
-        static mut VEHICLE_NAME: [u8; 64] = [b'\0'; 64];
-        imgui_sys::igInputText(
-            SPAWN_VEHICLE_MODAL.as_ptr() as *const i8,
-            VEHICLE_NAME.as_mut_ptr() as *mut i8,
-            8 * 64,
-            0,
-            None,
-            std::ptr::null_mut(),
-        );
-
-        static mut VEHICLE_COLOR_PRIMARY: i32 = 112;
-        static mut VEHICLE_COLOR_SECONDARY: i32 = 112;
-        imgui_sys::igInputInt(std::ffi::CString::new("Color1").unwrap().as_ptr(),
-            &mut VEHICLE_COLOR_PRIMARY, 1, 1, 0);
-        imgui_sys::igInputInt(std::ffi::CString::new("Color2").unwrap().as_ptr(),
-            &mut VEHICLE_COLOR_SECONDARY, 1, 1, 0);
-
-        imgui_sys::igSeparator();
-
-        let done_text = std::ffi::CString::new("Done").unwrap();
-        if imgui_sys::igButton(done_text.as_ptr(), imgui_sys::ImVec2::new(120.0, 0.0)) {
-            crate::thread_jumper::run(|world, _| {
-                let model_name_raw = std::ffi::CStr::from_bytes_with_nul_unchecked(&VEHICLE_NAME);
-                let model_name = model_name_raw
-                    .to_str()
-                    .unwrap()
-                    .trim_matches(char::from(0))
-                    .to_owned();
-
-                let player_ped_id = hook::natives::player::player_ped_id();
-                let position = hook::natives::entity::get_entity_coords(player_ped_id, true);
-                let rotation = hook::natives::entity::get_entity_rotation(player_ped_id, 2);
-
-                let (handle, model, transform, vehicle) =
-                    crate::entities::create_vehicle_with_model_name(
-                        &model_name,
-                        position,
-                        rotation,
-                        VEHICLE_COLOR_PRIMARY,
-                        VEHICLE_COLOR_SECONDARY,
-                    );
-
-                hook::natives::ped::set_ped_into_vehicle(player_ped_id, handle.handle, -1);
-
-                world.spawn((handle, model, transform, vehicle));
-
-                add_log(&format!("Spawned vehicle ({})", model_name.to_uppercase()));
-                VEHICLE_NAME = [b'\0'; 64];
-            });
-
-            imgui_sys::igCloseCurrentPopup();
-        }
-
-        imgui_sys::igEndPopup();
     }
 }
 
